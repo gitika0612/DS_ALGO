2f831ba7c9e8ba5f6a5ea90836a0ddcf003215dc
gitika0612 <bhatiagitika0612@gmail.com>
2021-05-10 15:35:08 +0530

    ReverseInt solution1

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,12 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+//solution1
+function reverseInt(n) {
+   const reverse = n.toString().split('').reverse().join('');
+   if(n < 0){
+       return parseInt(reverse) * -1;
+   } return parseInt(reverse);
+}
 
 module.exports = reverseInt;
