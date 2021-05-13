3dd51b01e4729266446dea634fd0c93714ead0b6
gitika0612 <bhatiagitika0612@gmail.com>
2021-05-13 14:49:45 +0530

    chunk solution2

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,19 +9,30 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 //solution1 
-function chunk(array, size) {
+//function chunk(array, size) {
+//    const chunked = [];
+//
+//    for (let element of array) {
+//        const last = chunked[chunked.length - 1];
+
+//        if (!last || last.length === size) {
+//            chunked.push([element]);
+//        } else {
+//            last.push(element);
+//        }
+//    }
+//    return chunked;
+//}
+
+//solution2
+function chunk(array, size){
     const chunked = [];
-
-    for (let element of array) {
-        const last = chunked[chunked.length - 1];
-
-        if (!last || last.length === size) {
-            chunked.push([element]);
-        } else {
-            last.push(element);
-        }
-    }
-    return chunked;
+    let index = 0;
+     while ( index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+     }
+     return chunked;
 }
 
 module.exports = chunk;
