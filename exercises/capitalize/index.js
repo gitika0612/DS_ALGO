090f1abfc474b712f625034d71e3396c7da10319
gitika0612 <bhatiagitika0612@gmail.com>
2021-05-15 16:08:20 +0530

    capitalize solution2

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,12 +8,27 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 //solution1
-function capitalize(str) {
-    const words = [];
-    for( let word of str.split(' ')){
-       words.push(word[0].toUpperCase() + word.slice(1));
+//function capitalize(str) {
+//    const words = [];
+//    for( let word of str.split(' ')){
+//       words.push(word[0].toUpperCase() + word.slice(1));
+//    }
+//    return words.join(' ');
+//}
+
+//solution2
+function capitalize(str){
+    let result = str[0].toUpperCase();
+
+    for ( let i = 1; i < str.length; i++){
+        if(str[i - 1] === ' '){
+            result += str[i].toUpperCase();
+        }else {
+            result += str[i];
+        }
     }
-    return words.join(' ');
+
+    return result;
 }
 
 module.exports = capitalize;
