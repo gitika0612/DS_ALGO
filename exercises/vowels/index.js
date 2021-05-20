b798aa1248ff0b09b65e6b30938d324676644493
gitika0612 <bhatiagitika0612@gmail.com>
2021-05-20 15:49:01 +0530

    vowels solution2

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,16 +8,22 @@
 //   vowels('Why?') --> 0
 
 //solution1
-function vowels(str) {
-    let count = 0;
-    const checker = ['a', 'e', 'i', 'o', 'u'];
+//function vowels(str) {
+//    let count = 0;
+//    const checker = ['a', 'e', 'i', 'o', 'u'];
+//
+//    for (let char of str.toLowerCase()){
+//        if(checker.includes(char)){
+//            count++;
+//        }
+//    }
+//    return count;
+//}
 
-    for (let char of str.toLowerCase()){
-        if(checker.includes(char)){
-            count++;
-        }
-    }
-    return count;
+//solution2
+function vowels(str){
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
