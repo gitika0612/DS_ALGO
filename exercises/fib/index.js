d1029d556a556ed0633f957b1116f2b3e849353d
gitika0612 <bhatiagitika0612@gmail.com>
2021-05-24 15:02:21 +0530

    fibonacci series solution2

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,17 +9,25 @@
 //   fib(4) === 3
 
 //solution1
+//function fib(n) {
+//    const result = [0, 1];
+//
+//    for ( i = 2 ; i <= n; i++){
+//        const a = result[i - 1];
+//        const b = result[i - 2];
+//
+//        result.push(a + b);
+//    }
+//
+//	return result[n];
+//}
+
+//solution2
 function fib(n) {
-    const result = [0, 1];
-
-    for ( i = 2 ; i <= n; i++){
-        const a = result[i - 1];
-        const b = result[i - 2];
-
-        result.push(a + b);
-    }
-
-		return result[n];
+	if (n < 2){
+		return n;
+	}
+		return fib(n - 1) + fib(n - 2)
 }
 
 module.exports = fib;
