3a3fab6357e0c20354385a6a5bf884fa563288db
gitika0612 <bhatiagitika0612@gmail.com>
2021-06-01 12:30:47 +0530

    memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -23,11 +23,34 @@
 //}
 
 //solution2
-function fib(n) {
-	if (n < 2){
+//function fib(n) {
+//	if (n < 2){
+//		return n;
+//	}
+//		return fib(n - 1) + fib(n - 2)
+//}
+
+module.exports = fib;
+
+// solution3
+function memoize(fn){
+	const cache = {};
+	return function(...args) {
+		if(cache[args]) {
+			return cache[args];
+		}
+		const result = fn.apply(this, args);
+		cache[args] = result;
+
+		return result;
+	};
+}
+
+function slowFib(n) {
+	if (n < 2) {
 		return n;
 	}
 		return fib(n - 1) + fib(n - 2)
 }
 
-module.exports = fib;
+const fib = memoize(slowFib);
