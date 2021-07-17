5b5b0206ab2a669f378e1a4e3676a18bd611117b
gitika0612 <bhatiagitika0612@gmail.com>
2021-07-17 13:29:56 +0530

    tree implementation

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -28,6 +28,34 @@ class Node {
 	}
 }
 
-class Tree {}
+class Tree {
+	constructor(){
+		this.root = null;
+	}
+
+	traverseBF(fn) {
+		const arr = [this.root];
+		while (arr.length) {
+			const node = arr.shift();
+
+			arr.push(...node.children);
+			fn(node);
+		}
+	}
+
+	traverseDF(fn) {
+		const arr = [this.root];
+		while (arr.length) {
+			const node = arr.shift();
+
+			arr.unshift(...node.children);
+			fn(node);
+		}
+	}
+}
+
+const node = new Node(1);
+const tree = new Tree();
+tree.root = node;
 
 module.exports = { Tree, Node };
